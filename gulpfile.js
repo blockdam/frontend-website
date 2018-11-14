07106b52a16c16475c08d95ba02caf9cd0389489
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-11-14 20:50:53 +0100

    update to package for node v11

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,8 @@
 'use strict';
 
 var gulp = require('gulp'),
-    gulpTaskLoader = require('gulp-task-loader')(), // load tasks from other files. Allows to write each gulp task into a separate file
-    // gulpSequence = require('gulp-sequence'), // sequences, allows for sequential execution of tasks instead of parallel tasks
-    watch = require('gulp-watch'); // watch plugin, also watches for new files and removed files
+    gulpTaskLoader = require('gulp-task-loader')();
 
 
     // configure which files to watch and what tasks to use on file changes
-    gulp.task('watch', function() {
-      gulp.watch(['scripts/*.js'], ['js-compile']);
-      gulp.watch(['stylesheets/*/*.scss','stylesheets/*.scss'], ['scss-compile']);
-    });
 
-gulp.task('default', ['watch']);
