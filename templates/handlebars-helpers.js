fd4d387080a061acdfed52754216e5e9e36b996b
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-11-25 16:49:28 +0100

    debug dependencies

--- templates/handlebars-helpers.js
+++ templates/handlebars-helpers.js
@@ -146,6 +146,7 @@ module.exports = [{
     {
         name: 'longTitle',
         helper: (content) => {
+
             if (content !== null && content !== undefined && content.length > 80) {
 
                 return 'longTitle';
