2b67b7ab72a2546f948dde2379df33b1332ba748
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-11-26 17:13:39 +0100

    activities added

--- templates/homepage/homepage.definition.js
+++ templates/homepage/homepage.definition.js
@@ -26,6 +26,7 @@ module.exports = {
         return new Promise((resolve, reject) => {
 
             let pagePersistence = new PagePersistence();
+            let discussionService = new DiscussionService();
 
 
             let postOptions = {
