aa232f0895b0e630b6e42b7ef598a8b8627d063c
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-04 20:30:40 +0100

    schermpje bax

--- scripts/metamask.js
+++ scripts/metamask.js
@@ -7,23 +7,31 @@ class MetaMask {
     init() {
 
 
-        window.addEventListener('load', function() {
-            // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-            if (typeof web3 !== 'undefined') {
-                // Use Mist/MetaMask's provider
-                web3js = new Web3(web3.currentProvider);
-                console.log('jaaa metamask');
-            } else {
-
-                console.log('nee geen metamask');
-                // Handle the case where the user doesn't have web3. Probably
-                // show them a message telling them to install Metamask in
-                // order to use our app.
-                // For example
-                // web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        window.addEventListener('load', async () => {
+            // Modern dapp browsers...
+            if (window.ethereum) {
+                window.web3 = new Web3(ethereum);
+                try {
+                    // Request account access if needed
+                    await ethereum.enable();
+                    // Acccounts now exposed
+                  //  web3.eth.sendTransaction({/* ... */});
+                    console.log(web3.eth);
+                } catch (error) {
+                    // User denied account access...
+                }
             }
-
-        })
+            // Legacy dapp browsers...
+            else if (window.web3) {
+                window.web3 = new Web3(web3.currentProvider);
+                // Acccounts always exposed
+                // web3.eth.sendTransaction({/* ... */});
+            }
+            // Non-dapp browsers...
+            else {
+                console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+            }
+        });
 
     }
 
