04d9b2b9b24fd3639648da67a5e12cc19a40cf4f
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-12 09:41:51 +0100

    bcd contract

--- scripts/metamask.js
+++ scripts/metamask.js
@@ -35,6 +35,8 @@ class MetaMask {
 
                     let bcd = web3.eth.contract(self.contractBCDToken.abi).at('0x788A378e7F82e36B3719644e042102d68BF597C7');
 
+                    console.log(bcd);
+
 
                     bcd.totalSupply.call(function (err, data) {
                         if (err) {
