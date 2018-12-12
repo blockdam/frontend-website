a3b2a8bc40ac4305e25f5c7807dfac1dc9aa805b
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-12 18:58:52 +0100

    bcd contract

--- scripts/charts.js
+++ scripts/charts.js
@@ -85,14 +85,14 @@ var Charts = function charts() {
                 top: 10,
                 bottom: 20,
                 left: 0,
-                right: 20
+                right: 0
             },
 
             padding: {
                 top: 0,
                 bottom: 0,
                 left: 0,
-                right: 0
+                right: 20
             }
         };
 
