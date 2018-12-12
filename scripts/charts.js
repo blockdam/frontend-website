d6dd0e1524da46d0a4cae3e57ce42a0c5551bb1b
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-12 18:59:42 +0100

    bcd contract

--- scripts/charts.js
+++ scripts/charts.js
@@ -56,7 +56,7 @@ var Charts = function charts() {
 
         layers.axis.append("g")
             .attr('class', 'total-axis')
-            .attr("transform", "translate(0," + (config.width - config.margin.right - config.padding.right) + ")")
+            .attr("transform", "translate(" + (config.width - config.margin.right - config.padding.right) + ",0)")
             .call(totalAxis);
     }
 
