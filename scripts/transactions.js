9e7e7f3a0e33122bb25eb1203759790d38f4af6c
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-19 21:02:46 +0100

    update json

--- scripts/transactions.js
+++ scripts/transactions.js
@@ -14,10 +14,37 @@ class Transactions {
         axios.get(url)
             .then(function (response) {
 
-                console.log(response.data);
+                self.createHTML(response.data);
 
                 //self.container
             });
+    }
+
+    createHTML(transactions) {
+
+        let table = document.createElement('table');
+
+        transactions.forEach((t) => {
+
+            let tr = document.createElement('tr');
+            let date = document.createElement('td');
+            date.innerHTML = t.date;
+            tr.appendChild(date);
+            let from = document.createElement('td');
+            from.innerHTML = t.from;
+            tr.appendChild(from);
+            let to = document.createElement('td');
+            from.innerHTML = t.to;
+            tr.appendChild(to);
+            let value = document.createElement('td');
+            value.innerHTML = t.value;
+            tr.appendChild(value);
+
+            table.appendChild(tr);
+
+        })
+
+        this.container.innerHTML = table;
     }
 
 }
