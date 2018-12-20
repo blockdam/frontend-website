da17898ee8dd752f872c387c80006c243aec7878
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-20 21:30:36 +0100

    updated gitignore

--- scripts/donate.js
+++ scripts/donate.js
@@ -47,16 +47,18 @@ class Donate {
 
         let self = this;
 
-        console.log(amount);
+        console.log(web3.eth.coinbase);
 
-        metaMask.bcdContract.transfer.sendTransaction(ethAddress, amount,{ from: web3.eth.coinbase }, function(err,receipt){
-            if (err) {
-                console.log(err)
-            }
-            if (receipt) {
-                self.tooltip.classList.remove('visible');
-            }
-        })
+        metaMask.bcdContract.transfer.sendTransaction(ethAddress, amount,{ from: web3.eth.coinbase });
+
+        // , function(err,receipt){
+        //     if (err) {
+        //         console.log(err)
+        //     }
+        //     if (receipt) {
+        //         self.tooltip.classList.remove('visible');
+        //     }
+        // })
     }
 }
 
