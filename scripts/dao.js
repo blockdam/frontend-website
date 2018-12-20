2abd7bf26e7557b24379dcf5b9aafccc64cbb901
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-20 19:23:14 +0100

    updated gitignore

--- scripts/dao.js
+++ scripts/dao.js
@@ -8,7 +8,36 @@ class Dao {
 
     init() {
 
+        let self = this,
+            url = 'https://blockdam.nl/smc-api/members';
 
+        axios.get(url)
+            .then(function (response) {
+                self.createHTML(response.data);
+            });
+    }
+
+    createHTML(transactions) {
+
+        let table = document.createElement('table');
+        table.classList.add('default');
+
+        transactions.reverse().forEach((m) => {
+
+            let tr = document.createElement('tr');
+
+            let name = document.createElement('td');
+            name.innerHTML = m.nickName;
+            tr.appendChild(name);
+
+            let address = document.createElement('td');
+            address.innerHTML = t.ethAddress;
+            tr.appendChild(address);
+
+            table.appendChild(tr);
+        });
+
+        this.container.appendChild(table);
     }
 
 }
