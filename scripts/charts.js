89d7ed3a1ad8a8c2adf7690cfdcfa01cd2dd357e
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-22 10:57:01 +0100

    updated gitignore

--- scripts/charts.js
+++ scripts/charts.js
@@ -131,6 +131,8 @@ var Charts = function charts() {
 
     let drawArea = function drawArea(data,config) {
 
+        console.log('once?');
+
         area = d3.area()
             .x0((d,i) => { return xScale(new Date(d.date))})
             .x1((d,i) => { return xScale(new Date(d.date))})
