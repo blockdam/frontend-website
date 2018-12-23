7a09b8986642d9d521eb331bcfc9ef803979768c
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-23 14:07:54 +0100

    updated gitignore

--- scripts/metamask.js
+++ scripts/metamask.js
@@ -9,6 +9,8 @@ class MetaMask {
         this.html.welcome = this.html.navItem.querySelector('#welcome_message');
         this.html.balance = this.html.navItem.querySelector('#personal_info');
         this.html.tooltip = this.html.navItem.querySelector('.tooltip');
+
+        this.bcdRatio = 1000000000000000000;
     }
 
 
@@ -16,48 +18,58 @@ class MetaMask {
 
         let self = this;
 
-            if (window.ethereum) {
-                window.web3 = new Web3(ethereum);
+        self._verifyMetaMask();
 
-                ethereum.enable();
+        // show tooltip with welcome message for several seconds. Will also hide when user scrolls
+        setTimeout( () => {
+            self._showTooltip();
+        },1000);
+        setTimeout( () => {
+            self._hideTooltip()
+        },9000);
+    }
 
-                this.html.navItem.style.display = 'flex';
+    _verifyMetaMask() {
 
-                if (web3.eth.accounts.length) {
+        let self = this;
 
-                    self.identify(web3.eth.accounts[0]);
+        if (!window.ethereum) {  // when user has the metamask addon, the browser tab has a global ethereum object
+            // user does not have the metaMask addon
+            self.html.welcome.innerHTML = 'Hello, we use metamask to connect to the smart contracts of the DAO'
+            // to do explore option to use our own web3 object
 
-                } else {
+        } else {
+            // user has the metaMask addon
+            // create web3 object
+            window.web3 = new Web3(ethereum);
+            ethereum.enable();
+            // check if user is connected to the correct network
+            if (web3.version.network  !== '4')  {
+                self.html.welcome.innerHTML = 'Hello, Our smart contracts are on the Rinkeby Testnet';
+                // check if metamask is not locked (private mode)
+            } else if (!web3.eth.accounts.length) {
+                self.html.welcome.innerHTML = 'Hello, Metamask may be locked'
+            } else {
+                // metaMask is functioning
+                // check if address of active metaMask account is on list of dao members
+                self.identify(web3.eth.accounts[0]);
 
-                    self.html.welcome.innerHTML = 'Hello, Metamask may be locked'
-
-                    setTimeout( () => {
-                        self.html.tooltip.classList.add('visible');
-                    },1000);
-                }
-
-                if (web3.version.network  !== '4')  {
-                    document.querySelector('#network_warning').style.display = 'block';
-                }
-
-
+                // connect to smart contracts
                 self.getBCDToken();
                 self.getBCDBondingCurve()
-
             }
+        }
+    }
 
-            else {
-                self.html.welcome.innerHTML = 'Hello, we use metamask to connect to the smart contracts of the DAO'
+    _showTooltip() {
+        this.html.tooltip.classList.add('visible');
+    }
 
-                setTimeout( () => {
-                    self.html.tooltip.classList.add('visible');
-                },1000);
-            }
+    _hideTooltip() {
+        this.html.tooltip.classList.remove('visible');
     }
 
     identify(address) {
-
-        console.log(address);
 
         let self = this,
             url = 'https://blockdam.nl/smc-api/members/' + address;
@@ -66,17 +78,12 @@ class MetaMask {
             .then(function (response) {
 
                 if(response.data !== null) {
-
                     self.html.welcome.innerHTML = 'Hello ' + response.data.nickName + '! The DAO welcomes you back.';
-
                 } else {
-
                     self.html.welcome.innerHTML = 'Hello! The DAO welcomes you back.';
                 }
 
-                setTimeout( () => {
-                    self.html.tooltip.classList.add('visible');
-                },1000);
+                self._showTooltip();
             });
     }
 
@@ -90,32 +97,32 @@ class MetaMask {
         axios.get(url)
             .then(function (response) {
 
+                // connect to contract
                 self.bcdContract = web3.eth.contract(response.data.abi).at(bcdTokenAddress);
 
+                // request total supply
                 if(self.html.totalSupply) {
                     self.bcdContract.totalSupply.call(function (err, data) {
                         if (err) {
                             console.log(err)
                         }
                         if (data) {
-                            self.html.totalSupply.innerHTML = data.toNumber() / 1000000000000000000;
+                            self.html.totalSupply.innerHTML = data.toNumber() / self.bcdRatio;
                         }
                     });
                 }
 
+                // request personal balance
                 self.bcdContract.balanceOf(web3.eth.coinbase, function (err, data) {
                     if (err) {
                         console.log(err)
                     }
                     if (data) {
-                        let val = data.toNumber() / 1000000000000000000;
+                        let val = data.toNumber() / self.bcdRatio;
                         self.html.balance.innerHTML = 'Your member address holds ' + val + ' BCD tokens';
                     }
                 });
-
             });
-
-
     }
 
     getBCDBondingCurve() {
@@ -127,6 +134,7 @@ class MetaMask {
         axios.get(url)
             .then(function (response) {
 
+                // connect to contract
                 let bcdBondingCurve = web3.eth.contract(response.data.abi).at(bcdBondingCurveAddress);
 
                 console.log(bcdBondingCurve);
