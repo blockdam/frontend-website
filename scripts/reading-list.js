670e4646d3e0d41878e6d7abfd9601a8fa300524
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-23 18:46:06 +0100

    updated gitignore

--- scripts/reading-list.js
+++ scripts/reading-list.js
@@ -19,7 +19,7 @@ class ReadingList {
                 // connect to contract
                 self.contract = web3.eth.contract(response.data.abi).at(address);
 
-                self.contract.slots(1).call(function (err, data) {
+                self.contract.slots.call(1,function (err, data) {
                     if (err) {
                         console.log(err)
                     }
