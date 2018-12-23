38cb0910f5845c0003fcf7bc72a1a7ff39deff04
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-23 17:24:14 +0100

    updated gitignore

--- scripts/reading-list.js
+++ scripts/reading-list.js
@@ -2,13 +2,24 @@ class ReadingList {
 
     constructor() {
 
-
+        this.contract = null;
     }
 
     init() {
 
-        // use web3 object attached to browser by metaMask
-        console.log(web3)
+        let self = this,
+            url = 'https://blockdam.nl/assets/smartcontracts/ReadingList.json',
+            address = '0xC09495D37DE2B74E1F01B461025D4c367bBB84b0';
+
+        axios.get(url)
+            .then(function (response) {
+
+                // connect to contract
+                self.contract = web3.eth.contract(response.data.abi).at(address);
+
+                console.log(self.contract);
+
+            });
     }
 }
 
