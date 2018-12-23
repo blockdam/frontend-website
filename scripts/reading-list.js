ba71d3f3d2f76cdcafa2ef6864a9d11dd4370e42
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-23 19:00:14 +0100

    updated gitignore

--- scripts/reading-list.js
+++ scripts/reading-list.js
@@ -11,13 +11,15 @@ class ReadingList {
 
         let self = this,
             url = 'https://blockdam.nl/assets/smartcontracts/ReadingList.json',
-            address = '0xC09495D37DE2B74E1F01B461025D4c367bBB84b0';
+            address = '0xefDb2303D3626490f00eA85C27879dDe1853e6e1';
 
         axios.get(url)
             .then(function (response) {
 
                 // connect to contract
                 self.contract = web3.eth.contract(response.data.abi).at(address);
+
+                console.log(self.contract);
 
                 self.contract.slots.call(1,function (err, data) {
                     if (err) {
