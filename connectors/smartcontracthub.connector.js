d9e7bb39e0c250ebaafae318ce0f71a6cb5fefc4
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-06 21:10:04 +0100

    ie11 fix

--- connectors/smartcontracthub.connector.js
+++ connectors/smartcontracthub.connector.js
@@ -1,10 +1,8 @@
 'use strict';
 const Promise = require('bluebird');
-const uuidv4 = require('uuid/v4');
 const requestify = require('requestify');
 const logger = require('../../services/logger.service');
 const config = require('../../config/index');
-const _ = require('lodash');
 
 
 class SmartContractHubConnector {
@@ -17,6 +15,7 @@ class SmartContractHubConnector {
             url = 'https://blockdam.nl/smc-api/reading-list';
 
         return new Promise((resolve, reject) => {
+            resolve();
             return requestify.get(url, {redirect: true, timeout: 120000})
             .then(response => {
                 resolve(response);
