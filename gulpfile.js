dfdef373bc0f1f2639e249d4b7a79263cbfb7458
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-08 22:05:52 +0100

    mopb menu

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@
 const gulp = require('gulp');
 const gulpTaskLoader = require('gulp-task-loader')('./gulp-tasks'); // load tasks from other files. Allows to write each gulp task into a separate file
 
-let build = gulp.parallel('scss-compile', function() {
-    // default task code here
-});
-
-gulp.task('default', build);
+// let build = gulp.parallel('scss-compile', function() {
+//     // default task code here
+// });
+//
+// gulp.task('default', build);
