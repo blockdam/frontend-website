ca10960881e5a26ed4ca36e173b70b6c944ceebe
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-20 19:43:24 +0100

    activity date automated

--- scripts/rsvp.js
+++ scripts/rsvp.js
@@ -12,7 +12,7 @@ class RSVP {
         self.meetupID = self.eventInfoBlock.getAttribute('data-meetup-id');
         console.log(self.meetupID);
 
-        let url = "https://api.meetup.com/2/events?offset=0&format=jsonp&limited_events=False&event_id=hsbcqqyzcbdc&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=10240746&sig=36567d847437b42c29337351433b7a";
+        let url = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&event_id=hsbcqqyzcbdc&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sign=true&sig_id=10240746&sig=36567d847437b42c29337351433b7a";
 
         axios.get(url)
             .then(function(response){
