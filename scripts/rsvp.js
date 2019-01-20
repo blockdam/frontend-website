0e4f203f76c4c2faa7a2b7307b8cd7696bbf6cf0
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-20 20:56:11 +0100

    activity date automated

--- scripts/rsvp.js
+++ scripts/rsvp.js
@@ -23,7 +23,8 @@ class RSVP {
         //     console.log(data.results[0].waitlist_count);
         // });
 
-        let rsvpUrl = self.addCallback("https://api.meetup.com/" + self.meetupUrlName + "/events/" + self.meetupID + "/rsvps");
+
+        let rsvpUrl = self.addCallback("https://api.meetup.com/" + self.meetupUrlName + "/events/" + self.meetupID + "/rsvps?&sign=true&photo-host=public");
 
         jsonpClient(rsvpUrl, function (err, data) {
             if(err) {
