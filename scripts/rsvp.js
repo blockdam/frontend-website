20ac1e6a5c253d6dd5424435b52691d4ca4eb5c9
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-20 19:19:01 +0100

    activity date automated

---
+++ scripts/rsvp.js
@@ -0,0 +1,18 @@
+class RSVP {
+
+    constructor() {
+
+        this.meetupApiKey = "36567d847437b42c29337351433b7a";
+    }
+
+    init() {
+
+
+
+    }
+
+
+}
+
+var rsvp = new RSVP();
+rsvp.init();
