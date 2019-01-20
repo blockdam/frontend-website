5337db1803e8c72714f273943918a421201e46d5
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-20 20:34:02 +0100

    activity date automated

--- scripts/rsvp.js
+++ scripts/rsvp.js
@@ -12,7 +12,7 @@ class RSVP {
         self.meetupID = self.eventInfoBlock.getAttribute('data-meetup-id');
         console.log(self.meetupID);
 
-        let url = self.addCallback("https://api.meetup.com/2/events?key=36567d847437b42c29337351433b7a&group_urlname=ny-techsign=true");
+        let url = self.addCallback("https://api.meetup.com/2/events?&sign=true&photo-host=public&event_id=hsbcqqyzcbdc&page=20");
 
         jsonpClient(url, function (err, data) {
             if(err) {
