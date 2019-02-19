ab7b29fb3b3dbe7c5e318bea5d5804b846383695
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-02-19 21:35:12 +0100

    dao

--- scripts/dao.js
+++ scripts/dao.js
@@ -13,17 +13,17 @@ class Dao {
         let self = this,
             url = 'https://blockdam.nl/smc-api/members';
 
-        this.checkPermissions(metaMask.userAddress).then( permissions => {
-
-            // if (permissions.minter) {
-            //     self.createMinter(permissions);
-            // }
-            //
-            // self.createVoucher(permissions);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        // this.checkPermissions(metaMask.userAddress).then( permissions => {
+        //
+        //     // if (permissions.minter) {
+        //     //     self.createMinter(permissions);
+        //     // }
+        //     //
+        //     // self.createVoucher(permissions);
+        // })
+        // .catch(function (error) {
+        //     console.log(error);
+        // });
     }
 
     createMemberList(members) {
@@ -55,82 +55,82 @@ class Dao {
         this.container.appendChild(table);
     }
 
-    checkPermissions(userAddress) {
+    // checkPermissions(userAddress) {
+    //
+    //     let self = this,
+    //         url = 'https://blockdam.nl/smc-api/dao/permissions';
+    //
+    //     return new Promise((resolve, reject) => {
+    //
+    //         axios.post(url, {
+    //             userAddress: userAddress
+    //         })
+    //             .then(function (response) {
+    //                 resolve(response.data);
+    //             })
+    //             .catch(function (error) {
+    //                 console.log(error);
+    //             });
+    //     });
+    // }
 
-        let self = this,
-            url = 'https://blockdam.nl/smc-api/dao/permissions';
+    // createMinter(permissions) {
+    //
+    //     let self = this,
+    //         minter = document.querySelector('#minter');
+    //
+    //     minter.style.display = 'flex';
+    //
+    //     minter.querySelector('.period').innerText = 'Mandated till ' + moment(permissions.minter.period).format('DD-MM-YYYY');
+    //     minter.querySelector('.budget').innerText = 'Budget: ' + permissions.minter.budget;
+    //
+    //     let form = minter.querySelector('form');
+    //
+    //     form.addEventListener("submit", function(event, errors) {
+    //         event.preventDefault();
+    //         if(errors) {
+    //             console.log(errors);
+    //         } else {
+    //             let address = form.querySelector("#address").value;
+    //             let amount = parseInt(form.querySelector("input[type='number']").value);
+    //             if (address && amount > 0) {
+    //                 self.mint(address, amount);
+    //             }
+    //         }
+    //     });
+    // }
 
-        return new Promise((resolve, reject) => {
-
-            axios.post(url, {
-                userAddress: userAddress
-            })
-                .then(function (response) {
-                    resolve(response.data);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        });
-    }
-
-    createMinter(permissions) {
-
-        let self = this,
-            minter = document.querySelector('#minter');
-
-        minter.style.display = 'flex';
-
-        minter.querySelector('.period').innerText = 'Mandated till ' + moment(permissions.minter.period).format('DD-MM-YYYY');
-        minter.querySelector('.budget').innerText = 'Budget: ' + permissions.minter.budget;
-
-        let form = minter.querySelector('form');
-
-        form.addEventListener("submit", function(event, errors) {
-            event.preventDefault();
-            if(errors) {
-                console.log(errors);
-            } else {
-                let address = form.querySelector("#address").value;
-                let amount = parseInt(form.querySelector("input[type='number']").value);
-                if (address && amount > 0) {
-                    self.mint(address, amount);
-                }
-            }
-        });
-    }
-
-    mint(address,amount) {
-
-        let self = this,
-            url = 'https://blockdam.nl/smc-api/dao/mint';
-
-
-        axios.post(url, {
-            'ethAddress': address,
-            'vouchers': amount
-        })
-        .then(function (response) {
-            console.log(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-    }
-
-    createVoucher(permissions) {
-
-        let self = this,
-            voter = document.querySelector('#voter'),
-            proposal = document.querySelector('#proposal');
-
-        voter.style.display = 'flex';
-        proposal.style.display = 'flex';
-
-        voter.querySelector('.amount').innerText = permissions.vouchers;
-    }
+    // mint(address,amount) {
+    //
+    //     let self = this,
+    //         url = 'https://blockdam.nl/smc-api/dao/mint';
+    //
+    //
+    //     axios.post(url, {
+    //         'ethAddress': address,
+    //         'vouchers': amount
+    //     })
+    //     .then(function (response) {
+    //         console.log(response.data);
+    //     })
+    //     .catch(function (error) {
+    //         console.log(error);
+    //     });
+    //
+    // }
+    //
+    // createVoucher(permissions) {
+    //
+    //     let self = this,
+    //         voter = document.querySelector('#voter'),
+    //         proposal = document.querySelector('#proposal');
+    //
+    //     voter.style.display = 'flex';
+    //     proposal.style.display = 'flex';
+    //
+    //     voter.querySelector('.amount').innerText = permissions.vouchers;
+    // }
 }
 
-var members = new Dao();
-members.init();
+var dao = new Dao();
+dao.init();
