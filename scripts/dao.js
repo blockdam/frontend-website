4eca49263631ce94a30e1adbe90ceeb7e7ecc5ed
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-02-19 19:13:41 +0100

    dao

--- scripts/dao.js
+++ scripts/dao.js
@@ -4,6 +4,7 @@ class Dao {
     constructor() {
 
         this.container = document.querySelector('#content-container');
+        this.aside = document.querySelector('aside');
         this.permissions = {};
     }
 
@@ -14,16 +15,23 @@ class Dao {
 
         this.permissions = this.checkPermissions(metaMask.userAddress);
 
+        if (this.permissions.minter) {
+            this.createMinter();
+        }
+
+        this.createVoucher(this.permissions.vouchers);
+
+
         let self = this,
             url = 'https://blockdam.nl/smc-api/members';
 
         axios.get(url)
             .then(function (response) {
-                self.createHTML(response.data);
+                self.createMemberList(response.data);
             });
     }
 
-    createHTML(members) {
+    createMemberList(members) {
 
         members.sort(function(a, b){
             if(a.nickName < b.nickName) { return 1; }
@@ -61,14 +69,31 @@ class Dao {
             userAddress: userAddress
         })
         .then(function (response) {
-            console.log(response.data);
-            // returns object with permissions
+
+            return response.data;
         })
         .catch(function (error) {
             console.log(error);
         });
 
     }
+
+    createMinter() {
+
+        let self = this;
+
+        let header = document.createElement('h3');
+        header.innerText('You are a minter');
+        self.aside.appendChild(header);
+
+    }
+
+    createVoucher() {
+
+    }
+
+
+}
 
 
 
