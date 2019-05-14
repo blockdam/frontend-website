9db82bbcef6086634e976ad44885d0a572ac3d27
Joera <joeramulders@Joeras-MacBook-Pro-21383.local>
2019-05-14 19:04:17 +0200

    linkje naar aragon

--- scripts/web3/bcd-token.js
+++ scripts/web3/bcd-token.js
@@ -6,27 +6,26 @@ let BCDToken = function BCDToken() {
     const bcdBondingCurveAddress = '0x307963cb5fce3bfceb30944bf0a65f7a2fe42b7e';
     const bcdTokenAddress = '0xA2F071aFe85e8F3ec51bD9ae5284Bf53204Df1b9';
     // let bcdContract = null;
-    // let bcdBondingCurve = null;
+    let bcdBondingCurve = null;
 
 
 
     const getContract = async function getContract() {
 
-        let url = 'https://blockdam.nl/assets/smartcontracts/bcdToken.json';
+        let response = await axios.get('https://blockdam.nl/assets/smartcontracts/bcdToken.json');
+        let abi = await response.data.abi;
+        let contract = await web3.eth.contract(response.data.abi).at(bcdTokenAddress);
+        return contract;
 
-        await axios.get(url)
-            .then(  (response) => {
-
-                return web3.eth.contract(response.data.abi).at(bcdTokenAddress);
-            });
     }
 
     const getBCDBondingCurve = async function getBCDBondingCurve() {
 
-        axios.get('https://blockdam.nl/assets/smartcontracts/bcdBondingCurve.json')
-            .then(function (response) {
-                bcdBondingCurve = web3.eth.contract(response.data.abi).at(bcdBondingCurveAddress);
-            });
+        let response = await axios.get('https://blockdam.nl/assets/smartcontracts/bcdBondingCurve.json');
+        let abi = await response.data.abi;
+
+        bcdBondingCurve = await web3.eth.contract(response.data.abi).at(bcdBondingCurveAddress);
+
     }
 
     const getSupply = async function getSupply(bcdContract) {
